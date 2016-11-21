1b38dfcc697934583ae616195e7da9a75f70745b
git4wmy <369666951@qq.com>
2016-11-21 23:15:00 +0800

    typeof and instanceof

---
+++ 2015_spring/my_task/task0002/util.js
@@ -0,0 +1,23 @@
+var arr = [1, 2, 3, 4];
+var str = "javascript";
+
+function isArray(arr) {
+    document.write(arr instanceof Array);
+    // return arr instanceof Array;
+}
+
+isArray(arr);
+document.write("<br />");
+
+isArray(str);
+document.write("<br />");
+
+function isFunction(fn) {
+    if (typeof fn === "function") {
+        document.write("is a function<br />");
+    }
+    else { document.write("is not a function<br />"); }
+}
+
+isFunction(arr);
+isFunction(isFunction);
