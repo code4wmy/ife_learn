2c226be36525d110342ff078cecc26cdda083ffd
git4wmy <369666951@qq.com>
2016-11-23 23:01:12 +0800

    uniqArray()

--- 2015_spring/my_task/task0002/util.js
+++ 2015_spring/my_task/task0002/util.js
@@ -21,3 +21,34 @@ function isFunction(fn) {
 
 isFunction(arr);
 isFunction(isFunction);
+
+function uniqArray(arr) {
+    var newArr = [];
+    //第一种
+    /*for(var i=0; i<arr.length;i++) {
+        if(arr.indexOf(arr[i])==i) 
+            newArr.push(arr[i]);
+    }
+    return newArr;
+    */
+
+    // 第二种
+    /*for(var i=0;i<arr.length;i++) {
+        if(newArr.indexOf(arr[i]) == -1)
+            newArr.push(arr[i]);
+    }
+    return newArr;
+    */
+
+    // 第三种
+    arr.sort();
+    for(var i=0;i<arr.length;i++) {
+        if(arr[i] != newArr[newArr.length - 1])
+            newArr.push(arr[i]);
+    }
+    return newArr;
+}
+
+var a = [1, 3, 5, 7, 5, 3, 1, 7, 11, "a", "b", "a"];
+var b = uniqArray(a);
+console.log(b);
