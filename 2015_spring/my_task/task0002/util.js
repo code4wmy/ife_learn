bf459c5a97b54b39d69a275b0cf7cb5b0e1514eb
git4wmy <369666951@qq.com>
2016-11-23 23:22:10 +0800

    cloneObject()

--- 2015_spring/my_task/task0002/util.js
+++ 2015_spring/my_task/task0002/util.js
@@ -52,3 +52,42 @@ function uniqArray(arr) {
 var a = [1, 3, 5, 7, 5, 3, 1, 7, 11, "a", "b", "a"];
 var b = uniqArray(a);
 console.log(b);
+
+// 使用递归来实现一个深度克隆，可以复制一个目标对象，返回一个完整拷贝
+// 被复制的对象类型会被限制为数字、字符串、布尔、日期、数组、Object对象。不会包含函数、正则对象等
+function isObject(obj){
+    if(Object.prototype.toString.call(obj)==='[object Array]' || Object.prototype.toString.call(obj)==='[object Object]')
+        return true;
+    else
+        return false;           
+}
+
+function cloneObject(obj){
+    var cloneObj;
+    cloneObj=(obj instanceof Array) ? []:{}; //判断对象类型，新建克隆对象
+    for(var i in obj){
+        if(obj.hasOwnProperty(i)){
+            cloneObj[i]=isObject(obj[i]) ? cloneObject(obj[i]):obj[i];
+        }
+    }
+    return cloneObj;
+}
+
+var srcObj = {
+    a: 1,
+    b: {
+        b1: ["hello", "hi"],
+        b2: "JavaScript"
+    }
+};
+var abObj = srcObj;
+var tarObj = cloneObject(srcObj);
+
+srcObj.a = 2;
+srcObj.b.b1[0] = "Hello";
+
+console.log(abObj.a);
+console.log(abObj.b.b1[0]);
+
+console.log(tarObj.a);      // 1
+console.log(tarObj.b.b1[0]);    // "hello"
